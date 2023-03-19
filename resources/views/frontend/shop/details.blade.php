30fb25580312ba78d679bb6c9b9de4a10b6995f7
AbdullahTgr <4multijob@gmail.com>
2023-03-19 13:33:36 +0200

    ini

--- resources/views/frontend/shop/details.blade.php
+++ resources/views/frontend/shop/details.blade.php
@@ -154,18 +154,7 @@
       </div>
       <!-- end breadcrumb -->
 
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9802244405698113"
-      crossorigin="anonymous"></script>
- <!-- product detail up -->
- <ins class="adsbygoogle"
-      style="display:block"
-      data-ad-client="ca-pub-9802244405698113"
-      data-ad-slot="8406198899"
-      data-ad-format="auto"
-      data-full-width-responsive="true"></ins>
- <script>
-      (adsbygoogle = window.adsbygoogle || []).push({});
- </script>
+      
 
 
       <div class="container container_cat cat_default">
